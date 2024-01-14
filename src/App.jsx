f14fe42bde6298d31d84131f493f2076751c7ad4
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-01-14 13:55:45 +0600

    Flight Search Task Project Created with  React,Tailwind  CSS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-5xl text-center'>
+        Flight Search
+      </h1>
+    </>
+  )
+}
+
+export default App
