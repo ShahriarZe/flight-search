b19bcd2b3ee6da2f32d0a11e39bf9a1292b18d72
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-01-17 16:09:55 +0600

    Footer third Added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import FooterMain from "./FooterMain";
 import FooterSecond from "./FooterSecond";
+import FooterThird from "./FooterThird";
 
 const Footer = () => {
     return (
         <div>
             <FooterMain></FooterMain>
             <FooterSecond></FooterSecond>
+            <FooterThird></FooterThird>
         </div>
     );
 };
---
+++ src/components/Footer/FooterThird.jsx
@@ -0,0 +1,15 @@
+
+
+const FooterThird = () => {
+    return (
+        <div>
+            <footer className="footer footer-center p-8 text-base-content font-bold">
+                <aside>
+                    <p>Copyright © 2024 - All right reserved by Flight Expert</p>
+                </aside>
+            </footer>
+        </div>
+    );
+};
+
+export default FooterThird;
